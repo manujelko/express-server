a9668ea1de865bc9ff81d0a65ec1455cfa448e87
Manuel Šarić <manujelko@gmail.com>
2023-02-22 17:21:04 +0100

    Adding a helper function for navigation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 
+const rootDir = require("./util/path");
+
 const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
@@ -12,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
