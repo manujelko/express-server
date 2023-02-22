35113c5b99df8e2c5daa994fc64b4342132bb9af
Manuel Šarić <manujelko@gmail.com>
2023-02-22 17:15:41 +0100

    Returning a 404 page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const bodyParser = require("body-parser");
 
@@ -11,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
