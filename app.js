ace1168894e090e172f53650ab05478ebc89868b
Manuel Šarić <manujelko@gmail.com>
2023-02-23 13:09:42 +0100

    Converting HTML files to pug

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(rootDir, "public")));
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render("404", { docTitle: "Page Not Found" });
 });
 
 app.listen(3000);
