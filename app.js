9362ce8180ad42d1b6860121984c1b49035eb47d
Manuel Šarić <manujelko@gmail.com>
2023-02-23 11:38:33 +0100

    Sharing data across requests & users

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const rootDir = require("./util/path");
 
 const bodyParser = require("body-parser");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
