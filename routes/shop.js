8a009aff2bef344b75d6f924cb42eb2870522c4f
Manuel Šarić <manujelko@gmail.com>
2023-02-23 12:06:10 +0100

    Outputting dynamic content

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
+const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("shop");
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
